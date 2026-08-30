21a6307af0a6956a2618acacb1d7472f4ef3ea03
Rishabh Bhardwaj <codingrishabh7@gmail.com>
2026-08-30 15:56:24 +0530

    Update race_loader.py

--- src/data/race_loader.py
+++ src/data/race_loader.py
@@ -2,7 +2,7 @@ import fastf1
 
 
 def load_race(year, grand_prix):
-    session = fastf1.get_session(year, grand_prix, "R")
+    session = fastf1.get_session(year, grand_prix, "R") #It's a FastF1 Session object representing that race session.
     session.load()  #It retrieves/processes the relevant race data and makes it accessible through the session object.
 
     return session 
